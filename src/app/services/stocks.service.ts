b995c105b32f1bb596f3506d62c0ec552b9952bc
Alex Levin <alex@email.com>
2018-08-19 22:27:56 +0100

    update

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -2,8 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
-let service: string = 'https://angular2-in-action-api.herokuapp.com';
+
 // endpoint URL: https://angular2-in-action-api.herokuapp.com/stocks/snapshot?symbols=aapl,goog,fb
+/*
+[{"symbol":"AAPL","lastTradePriceOnly":217.58,"change":4.2599945,"changeInPercent":0.019969972},
+{"symbol":"GOOG","lastTradePriceOnly":1200.96,"change":-5.5300293,"changeInPercent":-0.0045835683},
+{"symbol":"FB","lastTradePriceOnly":173.8,"change":-0.8999939,"changeInPercent":-0.0051516537}]
+ */
+// endpoint URL: https://angular2-in-action-api.herokuapp.com/stocks/historical/GOOG
+/*
+[{"date":"2018-08-17T04:00:00.000Z","open":1202.030029,"high":1209.02002,"low":1188.23999,"close":1200.959961,"adjClose":1200.959961,"volume":1381900,"symbol":"GOOG"},
+{"date":"2018-08-16T04:00:00.000Z","open":1224.72998,"high":1226,"low":1202.550049,"close":1206.48999,"adjClose":1206.48999,"volume":1343200,"symbol":"GOOG"},
+{"date":"2018-08-15T04:00:00.000Z","open":1229.26001,"high":1235.23999,"low":1209.51001,"close":1214.380005,"adjClose":1214.380005,"volume":1828800,"symbol":"GOOG"},
+...
+ */
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
 
 export interface StockInterface {
   symbol: string;
